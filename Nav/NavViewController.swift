c022876a7887b7e5912a1d027614edf77686588c
yyy <g03838@gmail.com>
2022-01-14 01:10:53 -0800

    first commit

---
+++ Nav/NavViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NavViewController.swift
+//  Nav
+//
+//  Created by 賴奕翔 on 2021/2/2.
+//
+
+import UIKit
+
+class NavViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
